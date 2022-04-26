c2a855298dfdd5a09398813b23dd8b9d17afbe4f
marcospaulo-4 <marcospaulo90castro@hotmail.com>
2022-04-26 12:49:45 -0300

    Create teste servidor webhook.js

---
+++ teste servidor webhook.js
@@ -0,0 +1,17 @@
+const { response } = require('express');
+const express = require ('express')
+const app = express();
+app.use(express.json());
+
+app.get('/',(request,response)=>{
+    return response.json({message : 'servidor esta rodando'})
+})
+
+app.post('/teste',(request,response)=>{
+    const {name,date} = request.body;
+
+    return response.json({name,date})
+})
+
+
+app.listen(3333)
